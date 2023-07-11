0bc312858bfe3f89c45661f7c6aa9ebe917a770d
= <fikretcansel25@hotmail.com>
2023-07-11 22:19:36 +0300

    cert test

--- app.js
+++ app.js
@@ -78,12 +78,10 @@ app.use((req, res) => {
 
 const port = 443;
 
-console.log(fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')))
-console.log(fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')))
 
 const sslServer = https.createServer({
-  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem'))
+  key: '',
+  cert: ''
 },
 app);
 
